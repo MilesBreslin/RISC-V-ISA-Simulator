de483a0e9fdabefab112a57a2ff371321857015a
Miles Breslin <milesbreslin@gmail.com>
2021-02-11 20:23:46 -0800

    Specify PC init

--- src/main.c
+++ src/main.c
@@ -1,28 +1,41 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
-bool debug = 0;
+bool verbose = 0;
 char* target_file = NULL;
+uint32_t pc_init = 0x00000000;
 
 void usage(char* message, int err) {
     FILE* fd = err == 0 ? stdout : stderr;
     fprintf(fd, "%s\n", message);
+
     fprintf(fd, "--load-file <config_file>          Decoded runtime code\n");
-    fprintf(fd, "--debug                            Show extra debug information\n");
+    fprintf(fd, "--pc-init <addr>                   Hexadecimal value to start the program counter (default: %08X)\n", pc_init);
+    fprintf(fd, "--verbose                          Show extra verbose information\n");
+
     fprintf(fd, "--help                             Show this help dialog\n");
     exit(err);
 }
 
 int main(int argc, char* argv[]) {
+    int err;
+
     // Parse Input Arguments
     for (int i = 1 ; i < argc ; i++) {
         if (strcmp("--load-file", argv[i]) == 0) {
             i++;
             target_file = argv[i];
-        } else if (strcmp("--debug", argv[i]) == 0) {
-            debug = true;
+        } else if (strcmp("--pc-init", argv[i]) == 0) {
+            i++;
+            if ((err = sscanf(argv[i], "%X", &pc_init)) != 1) {
+                fprintf(stderr, "Invalid format for --pc-init\n");
+            }
+        } else if (strcmp("--verbose", argv[i]) == 0) {
+            verbose = true;
         } else if (strcmp("--help", argv[i]) == 0) {
             usage("", 0);
         } else {
@@ -35,9 +48,9 @@ int main(int argc, char* argv[]) {
 
     // Load the Input file
     if (target_file) {
+        printf("pc-init at: %08X\n", pc_init);
         printf("Load file not implemented: %s\n", target_file);
     } else {
-        printf("Target file not specified\n");
-        return 1;
+        usage("Target file not specified", 1);
     }
 }
