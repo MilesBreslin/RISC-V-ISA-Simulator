06576cc4f9aafc673c74b8c5d199d8abd760e6e8
Abdullah <an36@pdx.edu>
2021-02-21 20:13:32 -0800

    BEQ fails due to opcode decoding

--- tests/instructions/beq_a0_a1_1024.c
+++ tests/instructions/beq_a0_a1_1024.c
@@ -1,5 +1,8 @@
 #include "test_main.h"
 int main() {
     // BEQ a0, a1, 1024
-    return !(IS_INSTRUCTION(0x40B50063, b, beq));
+    //return !(IS_INSTRUCTION(0x40B50063, b, beq));
+    B_INSTRUCTION decoded_instruction = as_b_instruction(0x40B50063);
+    printf("opcode: %02X\n func3: %01X\n", decoded_instruction.opcode, decoded_instruction.func3);
+    return is_beq_instruction(&decoded_instruction);
 }
