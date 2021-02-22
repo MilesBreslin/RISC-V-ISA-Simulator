5cccafe97b99f8fc38996f144e272802981ecf97
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-21 19:22:01 -0800

    created sll bad instruction test

--- tests/instructions/sll_zero_a5.c
+++ tests/instructions/sll_zero_a5.c
@@ -1,5 +1,5 @@
 #include "test_main.h"
 int main() {
-    // SUB a5, zero, 5
-    return !(IS_INSTRUCTION(0x405007b3, r, sub));
+    // SLL a5, zero, 5 bad instruction code
+    return (IS_INSTRUCTION(0x405007b3, r, sll));
 }
