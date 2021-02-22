63d20770d960242e5a8eebb3b85e9ca93de5b8cb
Abdullah <an36@pdx.edu>
2021-02-22 13:49:59 -0800

    BNE test was failing because it was compared to a BEQ for checking

--- tests/instructions/bne_s5_t1_360.c
+++ tests/instructions/bne_s5_t1_360.c
@@ -1,5 +1,8 @@
 #include "test_main.h"
 int main() {
     // BNE s5, t1, 360
-    return !(IS_INSTRUCTION(0x2C6A9863, b, beq));
+   // B_INSTRUCTION decoded = as_b_instruction(0x2C6A9863);
+    //printf("op:0x%02X\nfunct3:0x%01X\nrs1:x%d\nrs2:x%d\nimm:%d\n",decoded.opcode,decoded.func3,decoded.rs1,decoded.rs2,decoded.imm);
+    //return is_bne_instruction(&decoded);
+    return !(IS_INSTRUCTION(0x2C6A9863, b, bne));
 }
