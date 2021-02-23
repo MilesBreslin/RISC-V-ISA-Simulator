8cd9924da117729673958fc405d900941cfed014
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-23 14:07:08 -0800

    create I instruction LB

---
+++ tests/instructions/lb_a4_a5.c
@@ -0,0 +1,12 @@
+#include "test_main.h"
+int main() {
+    // LB a4, a5
+    uint32_t encoded = 0x00070703;
+    I_INSTRUCTION decoded = as_i_instruction(encoded);
+    if (!is_lb_instruction(&decoded))
+        FAIL("Not an LB  instruction");
+    VALUE_CHECK(decoded, imm, 0);
+    VALUE_CHECK(decoded, rd, REG_A4);
+    VALUE_CHECK(decoded, rs1, REG_A5);
+    return 0;
+}
