5ece86200cccfb913856439a1fc6f5524ba76427
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-23 05:04:28 -0800

    create I instruction tests for andi, ori, slli

---
+++ tests/instructions/slli_a5_zero_5.c
@@ -0,0 +1,12 @@
+#include "test_main.h"
+int main() {
+    // SLLI a5, zero, 5
+    uint32_t encoded = 0x00502793;
+    I_INSTRUCTION decoded = as_i_instruction(encoded);
+    if (!is_slli_instruction(&decoded))
+        FAIL("Not an slli instruction");
+    VALUE_CHECK(decoded, imm, 5);
+    VALUE_CHECK(decoded, rd, REG_A5);
+    VALUE_CHECK(decoded, rs1, REG_ZERO);
+    return 0;
+}
