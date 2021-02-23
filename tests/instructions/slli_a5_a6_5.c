659d49a4cea71e27f9c5b02b41755ae27ba1173c
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-23 06:16:18 -0800

    attempt to fix slli test

--- tests/instructions/slli_a5_a6_5.c
+++ tests/instructions/slli_a5_a6_5.c
@@ -1,12 +1,12 @@
 #include "test_main.h"
 int main() {
-    // SLLI a5, zero, 5
-    uint32_t encoded = 0x00501793;
+    // SLLI a5, a6, 5
+    uint32_t encoded = 0x00581793;
     I_INSTRUCTION decoded = as_i_instruction(encoded);
     if (!is_slli_instruction(&decoded))
         FAIL("Not an slli instruction");
     VALUE_CHECK(decoded, imm, 5);
     VALUE_CHECK(decoded, rd, REG_A5);
-    VALUE_CHECK(decoded, rs1, REG_ZERO);
+    VALUE_CHECK(decoded, rs1, REG_A^);
     return 0;
 }
