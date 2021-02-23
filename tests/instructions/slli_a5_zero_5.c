8ff22bebeedb87f1bd217be9ca5a2f351d6cbc51
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-23 05:18:08 -0800

    correct slli test instruction value

--- tests/instructions/slli_a5_zero_5.c
+++ tests/instructions/slli_a5_zero_5.c
@@ -1,7 +1,7 @@
 #include "test_main.h"
 int main() {
     // SLLI a5, zero, 5
-    uint32_t encoded = 0x00502793;
+    uint32_t encoded = 0x00501793;
     I_INSTRUCTION decoded = as_i_instruction(encoded);
     if (!is_slli_instruction(&decoded))
         FAIL("Not an slli instruction");
