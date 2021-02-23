26d66bde7d4c758941be87cde69b65dd3908b1af
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-23 14:26:02 -0800

    create I instruction LBU, LHU

---
+++ tests/instructions/lbu_a5_a5.c
@@ -0,0 +1,12 @@
+#include "test_main.h"
+int main() {
+    // LBU a5, a5
+    uint32_t encoded = 0x0007C783;
+    I_INSTRUCTION decoded = as_i_instruction(encoded);
+    if (!is_lbu_instruction(&decoded))
+        FAIL("Not an LBU  instruction");
+    VALUE_CHECK(decoded, imm, 0);
+    VALUE_CHECK(decoded, rd, REG_A5);
+    VALUE_CHECK(decoded, rs1, REG_A5);
+    return 0;
+}
