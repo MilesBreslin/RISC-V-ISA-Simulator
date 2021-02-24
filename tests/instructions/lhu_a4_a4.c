b1f5e61b6d3de9c225e0746ca44ca20ed993b403
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-23 17:44:28 -0800

    corrected lhu test case

--- tests/instructions/lhu_a4_a4.c
+++ tests/instructions/lhu_a4_a4.c
@@ -1,12 +1,12 @@
 #include "test_main.h"
 int main() {
-    // LHU a4, a4
+    // LHU a5, a4
     uint32_t encoded = 0x0007D783;
     I_INSTRUCTION decoded = as_i_instruction(encoded);
     if (!is_lhu_instruction(&decoded))
         FAIL("Not an LHU instruction");
     VALUE_CHECK(decoded, imm, 0);
-    VALUE_CHECK(decoded, rd, REG_A4);
+    VALUE_CHECK(decoded, rd, REG_A5);
     VALUE_CHECK(decoded, rs1, REG_A4);
     return 0;
 }
