897d8ffa25e3d1bad9bcf94a5b89847a53d3e99b
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-23 22:31:10 -0800

    corrections to instruction sb, sh ,sw

--- tests/instructions/sb_a5_5_s5.c
+++ tests/instructions/sb_a5_5_s5.c
@@ -2,10 +2,10 @@
 int main() {
     // SB a5, 5(s5)
     uint32_t encoded = 0x00FB02A3;
-    I_INSTRUCTION decoded = as_s_instruction(encoded);
+    I_INSTRUCTION decoded = is_s_instruction(encoded);
     if (!is_sb_instruction(&decoded))
         FAIL("Not a SB  instruction");
-    VALUE_CHECK(decoded, imm, 0);
+    VALUE_CHECK(decoded, imm, 5);
     VALUE_CHECK(decoded, rd, REG_S5);
     VALUE_CHECK(decoded, rs1, REG_A5);
     return 0;
---
+++ tests/instructions/sh_a5_5_s5.c
@@ -0,0 +1,12 @@
+#include "test_main.h"
+int main() {
+    // SH a5, 5(s5)
+    uint32_t encoded = 0x00FB12A3;
+    I_INSTRUCTION decoded = as_s_instruction(encoded);
+    if (!is_sh_instruction(&decoded))
+        FAIL("Not a SH  instruction");
+    VALUE_CHECK(decoded, imm, 0);
+    VALUE_CHECK(decoded, rd, REG_S5);
+    VALUE_CHECK(decoded, rs1, REG_A5);
+    return 0;
+}
