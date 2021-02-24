972eb6998366094c9f56df84e6c5c20319ad65b7
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-23 20:44:44 -0800

    create sb s instruction test

---
+++ tests/instructions/sb_a5_5_s5.c
@@ -0,0 +1,12 @@
+#include "test_main.h"
+int main() {
+    // SB a5, 5(s5)
+    uint32_t encoded = 0x00FB02A3;
+    I_INSTRUCTION decoded = as_s_instruction(encoded);
+    if (!is_sb_instruction(&decoded))
+        FAIL("Not a SB  instruction");
+    VALUE_CHECK(decoded, imm, 0);
+    VALUE_CHECK(decoded, rd, REG_S5);
+    VALUE_CHECK(decoded, rs1, REG_A5);
+    return 0;
+}
