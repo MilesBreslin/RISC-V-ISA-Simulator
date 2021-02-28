a3dbe4a6b923c4b9415ec7706ed761f76bd2d827
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-28 14:09:33 -0800

    corrects to or, and , xor, add, and sub

--- src/simulator.c
+++ src/simulator.c
@@ -174,14 +174,14 @@ bool execute_simulation_step(simulator* s) {
     if (is_add_instruction(&r_instruction)) {
         INFO("Instruction: ADD %d %d %d", r_instruction.rd, r_instruction.rs1, r_instruction.rs2);
         write_register(s, r_instruction.rd, 
-            (read_register(s, r_instruction.rs1) + r_instruction.rs2)
+            (read_register(s, r_instruction.rs1) + read_register(s, r_instruction.rs2)
         );  
         return true;
     }
     if (is_sub_instruction(&r_instruction)) {
         INFO("Instruction: SUB %d %d %d", r_instruction.rd, r_instruction.rs1, r_instruction.rs2);
         write_register(s, r_instruction.rd, 
-            (read_register(s, r_instruction.rs1)^ r_instruction.rs2)
+            read_register(s, r_instruction.rs1)^ read_register(s, r_instruction.rs2)
         );  
         return true;
     }
@@ -200,7 +200,7 @@ bool execute_simulation_step(simulator* s) {
     if (is_xor_instruction(&r_instruction)) {
         INFO("Instruction: XOR %d %d %d", r_instruction.rd, r_instruction.rs1, r_instruction.rs2);
         write_register(s, r_instruction.rd, 
-            read_register(s, r_instruction.rs1)^ r_instruction.rs2
+            read_register(s, r_instruction.rs1)^ read_register(s, r_instruction.rs2)
         );
         return true;
     }
@@ -215,14 +215,14 @@ bool execute_simulation_step(simulator* s) {
     if (is_or_instruction(&r_instruction)) {
         INFO("Instruction: OR %d %d %d", r_instruction.rd, r_instruction.rs1, r_instruction.rs2);
         write_register(s, r_instruction.rd, 
-            read_register(s, r_instruction.rs1) | r_instruction.rs2
+            read_register(s, r_instruction.rs1) | read_register(s, r_instruction.rs2)
         );
         return true;
     }
     if (is_and_instruction(&r_instruction)) {
         INFO("Instruction: AND %d %d %d", r_instruction.rd, r_instruction.rs1, r_instruction.rs2);
         write_register(s, r_instruction.rd, 
-            read_register(s, r_instruction.rs1) & r_instruction.rs2
+            read_register(s, r_instruction.rs1) & read_register(s, r_instruction.rs2)
         );
         return true;
     }
