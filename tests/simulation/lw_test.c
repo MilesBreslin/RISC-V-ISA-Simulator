6601d6f2c2a40e80dd51d531d808f4577c530c9f
DrFoz <tfosdick@ada.cs.pdx.edu>
2021-02-28 18:51:54 -0800

    First attempt at lw_test.c

---
+++ tests/simulation/lw_test.c
@@ -0,0 +1,16 @@
+#define PC_INIT 0
+#define SIMULATION_SIZE 1 << 20
+#define LOAD_FILE "lw_test.mem"
+#include "test_main.h"
+int simulation_run(simulator* s) {
+    // LW a5, a5
+    write_word(s, 0, 0x0007A783);
+    if (!execute_simulation_step(s))
+        FAIL("Simulation stopped unexpectedly");
+    //unit32_t value = read_register(s, REG_A5);
+    display_memory(s, 0, 16);
+    display_registers(s);
+    //if(value != 5)
+    //    FAIL("Expected 5, got %d", value);
+    return 0;
+}
