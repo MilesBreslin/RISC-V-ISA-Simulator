9a31241e0eb5a0e10984acf62e43d5076dc83b38
DrFoz <tfosdick@pdx.edu>
2021-03-04 01:08:01 -0800

    corrected MULH test

--- tests/simulation/mulh_x5_x6.c
+++ tests/simulation/mulh_x5_x6.c
@@ -1,13 +1,13 @@
 #include "test_main.h"
 int simulation_run(simulator* s) {
     // MULH S1, T0, T1 
-    write_register(s, REG_T0, 65535);
-    write_register(s, REG_T1, 65535);
+    write_register(s, REG_T0, 655);
+    write_register(s, REG_T1, 655);
     write_word(s, 0, 0x026294b3);
     if(!execute_simulation_step(s))
         FAIL("program ended prematurely");
     uint32_t value = read_register(s, REG_S1);
-    if (value != 65534) 
-        FAIL("Expected 18, got %d", value);
+    if (value != 0) 
+        FAIL("Expected 0, got %d", value);
     return 0;
 }
