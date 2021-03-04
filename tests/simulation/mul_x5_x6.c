bedb33c550f45aff7e5cfc2fdd9c0d413f68f8c9
DrFoz <tfosdick@pdx.edu>
2021-03-03 23:37:15 -0800

    added simulation test for MUL

---
+++ tests/simulation/mul_x5_x6.c
@@ -0,0 +1,12 @@
+#include "test_main.h"
+int simulation_run(simulator* s) {
+    // MUL S1, T0, T1 
+    write_register(s, REG_T0, 9);
+    write_register(s, REG_T1, 2);
+    write_word(s, 0, 0x026284b3);
+    execute_simulation_step(s);
+    uint32_t value = read_register(s, REG_A5);
+    if (value != 18) 
+        FAIL("Expected 18, got %d", value);
+    return 0;
+}
