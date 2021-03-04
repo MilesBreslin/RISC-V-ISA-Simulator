ed304e461ab7c6e53f8eb6f89db93f909898bc79
Abdullah Almarzouq <an36@pdx.edu>
2021-03-04 15:19:47 -0800

    Improved the mulh_x5_x6.c test + Fixed MULH operation

--- src/simulator.c
+++ src/simulator.c
@@ -524,7 +524,7 @@ bool execute_simulation_step(simulator* s) {
         return true;
     }
     if (is_mulh_instruction(&r_instruction)) {
-        int64_t value = ((int64_t)read_register_signed(s, r_instruction.rs1) * (int64_t)read_register_signed(s, r_instruction.rs2));
+        int64_t value = ((int64_t)read_register(s, r_instruction.rs1) * (int64_t)read_register(s, r_instruction.rs2));
         write_register_signed(s, r_instruction.rd, value>>32);
         return true;
     }
--- tests/simulation/mulh_x5_x6.c
+++ tests/simulation/mulh_x5_x6.c
@@ -2,12 +2,12 @@
 int simulation_run(simulator* s) {
     // MULH S1, T0, T1 
     write_register(s, REG_T0, 655);
-    write_register(s, REG_T1, 655);
+    write_register(s, REG_T1, -655);
     write_word(s, 0, 0x026294b3);
     if(!execute_simulation_step(s))
         FAIL("program ended prematurely");
     uint32_t value = read_register(s, REG_S1);
-    if (value != 429025) 
+    if (value != 654) 
         FAIL("Expected 429025, got %d", value);
     return 0;
 }
