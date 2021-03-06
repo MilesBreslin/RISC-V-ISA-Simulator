21ccf1eb1fa8316b903dbe3b2f9a3ce10e381f51
DrFoz <tfosdick@pdx.edu>
2021-03-05 21:06:53 -0800

    created andi simulation test

--- tests/simulation/andi_x5_5.c
+++ tests/simulation/andi_x5_5.c
@@ -1,13 +1,12 @@
 #include "test_main.h"
 int simulation_run(simulator* s) {
-    // MUL  S1, T0, T1 
-    write_register(s, REG_T0, 2);
-    write_register(s, REG_T1, 9);
-    write_word(s, 0, 0x026284b3);
+    // ANDI  S1, T0, 5 
+    write_register(s, REG_T0, 24);
+    write_word(s, 0, 0x0052f493);
     if(!execute_simulation_step(s))
         FAIL("program ended prematurely");
     uint32_t value = read_register(s, REG_S1);
-    if (value != 18) 
-        FAIL("Expected 18, got %d", value);
+    if (value != 29) 
+        FAIL("Expected 29, got %d", value);
     return 0;
 }
