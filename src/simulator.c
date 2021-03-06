3d252e77ceda6bb26531c10a4cf250ab32f96cf6
DrFoz <tfosdick@pdx.edu>
2021-03-05 21:35:14 -0800

    still playing with andi

--- src/simulator.c
+++ src/simulator.c
@@ -391,9 +391,9 @@ bool execute_simulation_step(simulator* s) {
         return true;
     }
     if (is_andi_instruction(&i_instruction)) {
-        write_register(s, i_instruction.rd, 
-            read_register(s, i_instruction.rs1)&((int32_t)i_instruction.imm_s)
-        );
+        uint32_t imm_value = (read_register_signed(s, i_instruction.rs1)&((int32_t)i_instruction.imm_s));
+        printf("The immediate value is : %d ", imm_value);
+        write_register_signed(s, i_instruction.rd, imm_value);
         return true;
     }
     if (is_slli_instruction(&i_instruction)) {
--- tests/simulation/andi_x5_5.c
+++ tests/simulation/andi_x5_5.c
@@ -1,14 +1,12 @@
 #include "test_main.h"
 int simulation_run(simulator* s) {
     // ANDI  S1, T0, 5 
-    write_register(s, REG_T0, 0x18);
+    write_register(s, REG_T0, 24);
     write_word(s, 0, 0x0052f493);
 
     if(!execute_simulation_step(s))
         FAIL("program ended prematurely");
     uint32_t value = read_register(s, REG_S1);
-    uint32_t imm_val = read_register_signed(s, i_instruction.imm_s);
-        INFO("Immediate value is: %d");
     if (value != 29) 
         FAIL("Expected 29, got %d", value);
     return 0;
