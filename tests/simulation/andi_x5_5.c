f495af1af27822eec4dcc77dd03f072c5e3a8747
DrFoz <tfosdick@pdx.edu>
2021-03-05 22:25:08 -0800

    fixed andi simulator test

--- tests/simulation/andi_x5_5.c
+++ tests/simulation/andi_x5_5.c
@@ -1,13 +1,13 @@
 #include "test_main.h"
 int simulation_run(simulator* s) {
-    // ANDI  S1, T0, 5 
-    write_register(s, REG_T0, 24);
+    // ANDI  S0, T0, 5 
+    write_register(s, REG_T0, 5);
     write_word(s, 0, 0x0052f493);
 
     if(!execute_simulation_step(s))
         FAIL("program ended prematurely");
-    uint32_t value = read_register(s, REG_S1);
-    if (value != 29) 
-        FAIL("Expected 29, got %d", value);
+    uint32_t value = read_register(s, REG_S0);
+    if (value != 5) 
+        FAIL("Expected 5, got %d", value);
     return 0;
 }
