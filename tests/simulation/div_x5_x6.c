f6488202e6f4ec7631f28c3e69a6397097c91c3e
DrFoz <tfosdick@pdx.edu>
2021-03-06 00:29:42 -0800

    playing with div simulation test

--- tests/simulation/div_x5_x6.c
+++ tests/simulation/div_x5_x6.c
@@ -1,7 +1,7 @@
 #include "test_main.h"
 int simulation_run(simulator* s) {
     // DIV S1, T0, T1 
-    write_register(s, REG_T0, 361);
+    write_register(s, REG_T0, 359);
     write_register(s, REG_T1, 5);
     write_word(s, 0, 0x0262c4b3);
     if(!execute_simulation_step(s))
