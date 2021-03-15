6d74ae69a5ef19437b8f0e00ffaf39f572890260
Miles Breslin <milesbreslin@gmail.com>
2021-03-15 14:16:23 -0700

    Include ECALL write bytes out

--- src/simulator.c
+++ src/simulator.c
@@ -617,7 +617,9 @@ bool execute_simulation_step(simulator* s) {
                 if (addr + length > s->mem_bytes)
                     length = s->mem_bytes - addr;
                 if (fd <= length(fds))
-                    fprintf(fds[fd], "%-*s", length, &((uint8_t*) s->memory)[addr]);
+                    write_reg(s, REG_A0,
+                        fprintf(fds[fd], "%-*s", length, &((uint8_t*) s->memory)[addr])
+                    );
                 return true;
             case 94:
                 INFO("ECALL: exit");
